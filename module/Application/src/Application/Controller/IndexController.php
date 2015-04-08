9c3a2c56fc35cd5de955c366a0e3141a039b25b9
alcatraz1907 <alcatrazua521@gmail.com>
2015-04-09 01:06:10 +0300

    Autentification is valid

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -49,51 +49,44 @@ class IndexController extends AbstractActionController
 
         if ($request->isPost()) {
 
-//- $authFormFilters = new User(); // we use the Entity for the filters
-// TODO fix the filters
-//- $form->setInputFilter($authFormFilters->getInputFilter());
-// Filters have been fixed
+            //- $authFormFilters = new User(); // we use the Entity for the filters
+            // TODO fix the filters
+            //- $form->setInputFilter($authFormFilters->getInputFilter());
+            // Filters have been fixed
 
             $form->setInputFilter(new LoginInputFilter($this->getServiceLocator()));
             $form->setData($request->getPost());
-// echo "<h1>I am here1</h1>";
+
             if ($form->isValid()) {
 
                 $data = $form->getData();
-// $data = $this->getRequest()->getPost();
-// If you used another name for the authentication service, change it here
-// it simply returns the Doctrine Auth. This is all it does. lets first create the connection to the DB and the Entity
+                // $data = $this->getRequest()->getPost();
+                // If you used another name for the authentication service, change it here
+                // it simply returns the Doctrine Auth.
+                //This is all it does. lets first create the connection to the DB and the Entity
                 $authService = $this->getServiceLocator()->get('Zend\Authentication\AuthenticationService');
-// Do the same you did for the ordinar Zend AuthService
+                // Do the same you did for the ordinar Zend AuthService
                 $adapter = $authService->getAdapter();
-                $adapter->setIdentityValue($data['login']); //$data['usr_name']
-                $adapter->setCredentialValue($data['password']); // $data['usr_password']
+                $adapter->setIdentityValue($data['login']);
+                $adapter->setCredentialValue($data['password']);
 
                 $authResult = $authService->authenticate();
-// echo "<h1>I am here</h1>";
-              //  die(print_r($authResult));
 
                 if ($authResult->isValid()) {
 
                     $identity = $authResult->getIdentity();
                     $authService->getStorage()->write($identity);
                     $time = 1209600; // 14 days 1209600/3600 = 336 hours => 336/24 = 14 days
-//- if ($data['rememberme']) $authService->getStorage()->session->getManager()->rememberMe($time); // no way to get the session
+                    //- if ($data['rememberme']) $authService->getStorage()->session->getManager()->rememberMe($time); // no way to get the session
                     if ($data['rememberme']) {
                         $sessionManager = new \Zend\Session\SessionManager();
                         $sessionManager->rememberMe($time);
                     }
-//- return $this->redirect()->toRoute('home');
+                //- return $this->redirect()->toRoute('home');
                 }
                 foreach ($authResult->getMessages() as $message) {
                     $messages .= "$message\n";
                 }
-                /*
-                $identity = $authenticationResult->getIdentity();
-                $authService->getStorage()->write($identity);
-                $authenticationService = $this->serviceLocator()->get('Zend\Authentication\AuthenticationService');
-                $loggedUser = $authenticationService->getIdentity();
-                */
             }
         }
 
@@ -102,5 +95,34 @@ class IndexController extends AbstractActionController
             'form'	=> $form,
             'messages' => $messages,
         ));
+    }
+
+    public function logoutAction()
+    {
+        // in the controller
+        // $auth = new AuthenticationService();
+        $auth = $this->getServiceLocator()->get('Zend\Authentication\AuthenticationService');
+        // @todo Set up the auth adapter, $authAdapter
+        if ($auth->hasIdentity()) {
+            // Identity exists; get it
+            $identity = $auth->getIdentity();
+//-			echo '<pre>';
+//-			print_r($identity);
+//-			echo '</pre>';
+        }
+        $auth->clearIdentity();
+//-		$auth->getStorage()->session->getManager()->forgetMe(); // no way to get to the sessionManager from the storage
+        $sessionManager = new \Zend\Session\SessionManager();
+        $sessionManager->forgetMe();
+
+        // $view = new ViewModel(array(
+        //    'message' => 'Hello world',
+        // ));
+        // $view->setTemplate('foo/baz-bat/do-something-crazy');
+        // return $view;
+
+        // return $this->redirect()->toRoute('home');
+        return $this->redirect()->toRoute('site/home', array('controller' => 'index', 'action' => 'login'));
+
     }
 }
