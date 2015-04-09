10b51e1ad9fbb84ad2279e582f013889254fa4af
alcatraz1907 <alcatrazua521@gmail.com>
2015-04-09 14:03:13 +0300

    Autentification is valid login , logout

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -9,28 +9,6 @@
 
 return array(
     'doctrine' => array(
-//        'authentication' => array( // this part is for the Auth adapter from DoctrineModule/Authentication
-//            'orm_default' => array(
-//                'object_manager' => 'Doctrine\ORM\EntityManager',
-//// object_repository can be used instead of the object_manager key
-//                'identity_class' => 'Application\Entity\Users', //'Application\Entity\User',
-//                'identity_property' => 'login', // 'username', // 'email',
-//                'credential_property' => 'password', // 'password',
-//                'credential_callable' => function( $user, $passwordGiven) { // not only User
-//// return my_awesome_check_test($user->getPassword(), $passwordGiven);
-//// echo '<h1>callback user->getPassword = ' .$user->getPassword() . ' passwordGiven = ' . $passwordGiven . '</h1>';
-////- if ($user->getPassword() == md5($passwordGiven)) { // original
-//// ToDo find a way to access the Service Manager and get the static salt from config array
-//                    if ($user->getPassword() == md5('aFGQ475SDsdfsaf2342' . $passwordGiven /*. $user->getUsrPasswordSalt()*/)/*&&
-//                        $user->getUsrActive() == 1*/){
-//                        return true;
-//                    }
-//                    else {
-//                        return false;
-//                    }
-//                },
-//            ),
-//        ),
         'authentication' => array(
             'orm_default' => array(
                 'object_manager' => 'Doctrine\ORM\EntityManager',
--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -49,23 +49,14 @@ class IndexController extends AbstractActionController
 
         if ($request->isPost()) {
 
-            //- $authFormFilters = new User(); // we use the Entity for the filters
             // TODO fix the filters
-            //- $form->setInputFilter($authFormFilters->getInputFilter());
-            // Filters have been fixed
-
             $form->setInputFilter(new LoginInputFilter($this->getServiceLocator()));
             $form->setData($request->getPost());
 
             if ($form->isValid()) {
 
                 $data = $form->getData();
-                // $data = $this->getRequest()->getPost();
-                // If you used another name for the authentication service, change it here
-                // it simply returns the Doctrine Auth.
-                //This is all it does. lets first create the connection to the DB and the Entity
                 $authService = $this->getServiceLocator()->get('Zend\Authentication\AuthenticationService');
-                // Do the same you did for the ordinar Zend AuthService
                 $adapter = $authService->getAdapter();
                 $adapter->setIdentityValue($data['login']);
                 $adapter->setCredentialValue($data['password']);
@@ -77,12 +68,12 @@ class IndexController extends AbstractActionController
                     $identity = $authResult->getIdentity();
                     $authService->getStorage()->write($identity);
                     $time = 1209600; // 14 days 1209600/3600 = 336 hours => 336/24 = 14 days
-                    //- if ($data['rememberme']) $authService->getStorage()->session->getManager()->rememberMe($time); // no way to get the session
+
                     if ($data['rememberme']) {
                         $sessionManager = new \Zend\Session\SessionManager();
                         $sessionManager->rememberMe($time);
                     }
-                //- return $this->redirect()->toRoute('home');
+                return $this->redirect()->toRoute('site/home');
                 }
                 foreach ($authResult->getMessages() as $message) {
                     $messages .= "$message\n";
@@ -99,28 +90,14 @@ class IndexController extends AbstractActionController
 
     public function logoutAction()
     {
-        // in the controller
-        // $auth = new AuthenticationService();
         $auth = $this->getServiceLocator()->get('Zend\Authentication\AuthenticationService');
         // @todo Set up the auth adapter, $authAdapter
         if ($auth->hasIdentity()) {
-            // Identity exists; get it
             $identity = $auth->getIdentity();
-//-			echo '<pre>';
-//-			print_r($identity);
-//-			echo '</pre>';
         }
         $auth->clearIdentity();
-//-		$auth->getStorage()->session->getManager()->forgetMe(); // no way to get to the sessionManager from the storage
         $sessionManager = new \Zend\Session\SessionManager();
         $sessionManager->forgetMe();
-
-        // $view = new ViewModel(array(
-        //    'message' => 'Hello world',
-        // ));
-        // $view->setTemplate('foo/baz-bat/do-something-crazy');
-        // return $view;
-
         // return $this->redirect()->toRoute('home');
         return $this->redirect()->toRoute('site/home', array('controller' => 'index', 'action' => 'login'));
 
